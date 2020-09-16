796abd82bc7dfd28bb09d016a6876ee2b88ae38e
Kevin ADDA <adda.kevin@gmail.com>
2020-09-16 18:44:49 +0200

    dev(entity-details) : use french as source localey

--- frontend/src/modules/entitydetails/components/EntityDetails.js
+++ frontend/src/modules/entitydetails/components/EntityDetails.js
@@ -101,7 +101,7 @@ export class EntityDetailsBase extends React.Component<InternalProps, State> {
         }
         console.log('machin?', selectedEntity.pk === otherlocales.entity && (!machinery || selectedEntity.pk !== machinery.entity),
         selectedEntity.pk, otherlocales.entity, machinery, otherlocales)
-        if (selectedEntity.pk === otherlocales.entity && !otherlocales.entity.fetching && (!machinery || selectedEntity.pk !== machinery.entity)) {
+        if (selectedEntity.pk === otherlocales.entity && !otherlocales.fetching && (!machinery || selectedEntity.pk !== machinery.entity)) {
             console.log('machin!')
             this.fetchMachineryData();
         }
